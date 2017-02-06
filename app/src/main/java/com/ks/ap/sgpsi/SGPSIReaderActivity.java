5c79b123bb76c9d16d3291876f5f1f76b2f6dc6c
kishorsutar <kishor007sutar@gmail.com>
2017-02-06 23:46:37 +0800

    Initial commit

---
+++ app/src/main/java/com/ks/ap/sgpsi/SGPSIReaderActivity.java
@@ -0,0 +1,13 @@
+package com.ks.ap.sgpsi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SGPSIReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgpsireader);
+    }
+}
