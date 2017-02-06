7fa8ca08599aa073e199a49af6fad05bc375ace5
kishorsutar <kishor007sutar@gmail.com>
2017-02-06 23:53:32 +0800

    First commit

--- app/src/main/java/com/ks/ap/sgpsi/SGPSIReaderActivity.java
+++ app/src/main/java/com/ks/ap/sgpsi/SGPSIReaderActivity.java
@@ -1,8 +1,11 @@
 package com.ks.ap.sgpsi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Created by kishorsutar on 2/6/17.
+ */
 public class SGPSIReaderActivity extends AppCompatActivity {
 
     @Override
